53ac6717a8d675ad6279878ab144adc42b918394
Eduardo Irías <eduardoirias@me.com>
2015-09-23 00:53:29 -0600

    Parse Integration
    Parse Integration with the project
    Parse Application Id and Client Id configured in the project

--- Game On Hackathon/AppDelegate.swift
+++ Game On Hackathon/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        Parse.enableLocalDatastore()
+        Parse.setApplicationId(ParseKeys.applicationId, clientKey: ParseKeys.clientKey)
+        PFAnalytics.trackAppOpenedWithLaunchOptions(launchOptions)
+        
         return true
     }
 
