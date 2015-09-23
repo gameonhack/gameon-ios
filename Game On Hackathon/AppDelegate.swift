81b9cf5ad49680548dcf43db384d48ef8a20ea24
Eduardo Irías <eduardoirias@me.com>
2015-09-23 08:19:08 -0600

    Facebook SDK
    Facebook SDK
    Facebook login

--- Game On Hackathon/AppDelegate.swift
+++ Game On Hackathon/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Parse
+import ParseFacebookUtilsV4
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,9 +18,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        //Parse
         Parse.enableLocalDatastore()
         Parse.setApplicationId(ParseKeys.applicationId, clientKey: ParseKeys.clientKey)
         PFAnalytics.trackAppOpenedWithLaunchOptions(launchOptions)
+        PFFacebookUtils.initializeFacebookWithApplicationLaunchOptions(launchOptions)
+
+        //Facebook
+        FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
         
         return true
     }
@@ -40,12 +46,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidBecomeActive(application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        FBSDKAppEvents.activateApp()
     }
 
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
+    func application(application: UIApplication, openURL url: NSURL, sourceApplication: String?, annotation: AnyObject) -> Bool {
+        return FBSDKApplicationDelegate.sharedInstance().application(application, openURL: url, sourceApplication: sourceApplication, annotation: annotation)
+    }
 
 }
 
---
+++ Game On Hackathon/LoginViewController.swift
@@ -0,0 +1,96 @@
+//
+//  LoginViewController.swift
+//  Game On Hackathon
+//
+//  Created by Eduardo Irías on 9/23/15.
+//  Copyright (c) 2015 Game On Hackathon. All rights reserved.
+//
+
+import UIKit
+import Parse
+import ParseFacebookUtilsV4
+
+class LoginViewController: UIViewController, FBSDKLoginButtonDelegate {
+
+    @IBOutlet weak var userNameLabel : UILabel!
+    @IBOutlet weak var userEmailLabel : UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let loginButton = FBSDKLoginButton()
+        loginButton.delegate = self
+        loginButton.readPermissions = ["public_profile","email", "user_friends"]
+        loginButton.center = self.view.center
+        self.view.addSubview(loginButton)
+        
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        refreshUserInfo ()
+    }
+
+    func refreshUserInfo () {
+        let user = PFUser.currentUser()
+        if (user != nil) {
+            if let name = user?.objectForKey("name") as? String {
+                userNameLabel.text = name
+            }
+            if let email = user?.objectForKey("email") as? String {
+                userEmailLabel.text = email
+            }
+        } else {
+            userNameLabel.text = ""
+            userEmailLabel.text = ""
+        }
+    }
+    
+    func linkInfo() {
+        PFFacebookUtils.logInInBackgroundWithAccessToken(FBSDKAccessToken.currentAccessToken()) { (user : PFUser?, error : NSError?) -> Void in
+            if let user = user {
+                if user.isNew {
+                    print("User signed up and logged in through Facebook!")
+                } else {
+                    print("User logged in through Facebook!")
+                }
+                let req = FBSDKGraphRequest(graphPath: "me", parameters:  ["fields":"email,name"])
+                req.startWithCompletionHandler({ (connection : FBSDKGraphRequestConnection!, result: AnyObject!, error: NSError!) -> Void in
+                    if let name = result["name"] as? String {
+                        user["name"] = name
+                        user.saveEventually()
+                    }
+                    if let email = result["email"] as? String {
+                        user["email"] = email
+                        user.saveEventually()
+                    }
+                    self.refreshUserInfo ()
+                })
+                
+            } else {
+                print("Uh oh. The user cancelled the Facebook login.")
+            }
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    //MARK: - FBSDKLoginButtonDelegate
+    
+    func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
+        if !result.isCancelled {
+            self.linkInfo()
+        }
+    }
+    func loginButtonDidLogOut(loginButton: FBSDKLoginButton!) {
+        PFUser.logOut()
+        refreshUserInfo ()
+    }
+    
+
+
+}
+
