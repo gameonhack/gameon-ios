2e91924352624f2f6b04eee6ee846a4c750dd212
Eduardo Irias <eduardoirias@me.com>
2017-01-16 01:32:47 -0600

    (っ˘▽˘)っ Game On ⊂(◕。◕⊂)
    The old project was really OLD. So we decided to create new one. :)

---
+++ Game On/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Game On
+//
+//  Created by Eduardo Irias on 1/16/17.
+//  Copyright © 2017 Game On. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
