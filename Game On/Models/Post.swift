ae16764489823b5652087e28349cd12c08bb69a3
Eduardo Irias <eduardoirias@me.com>
2017-01-19 11:58:51 -0600

    Post Model
    Post model is a PFObject implementing PFSubclassing protocol.

---
+++ Game On/Models/Post.swift
@@ -0,0 +1,23 @@
+//
+//  Post.swift
+//  Game On
+//
+//  Created by Eduardo Irias on 1/19/17.
+//  Copyright © 2017 Game On. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class Post: PFObject, PFSubclassing {
+
+    static func parseClassName() -> String {
+        return "Post"
+    }
+    
+    @NSManaged var user : PFUser!
+    @NSManaged var content : String!
+    @NSManaged var image : UIImage?
+    
+    
+}
