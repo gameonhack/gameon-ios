b3ff79bcdc7689de6c3b4cb3f10be5217fd758b5
beito <beito_xp@me.com>
2017-02-17 00:50:18 -0600

    Group Table View
    Tableview for the Group Controller. Using UITableViewDelegate and UITableViewDataSource. Linked to the Groups Model

---
+++ Game On/Controllers/Groups/GroupsViewController.swift
@@ -0,0 +1,71 @@
+//
+//  GroupsViewController.swift
+//  Game On
+//
+//  Created by Julio Marín on 11/2/17.
+//  Copyright © 2017 Game On. All rights reserved.
+//
+
+import UIKit
+
+class GroupsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var groups = [Group](){
+        didSet{
+            tableView.reloadData()
+        }
+    }
+    
+    @IBOutlet var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.tableView.tableFooterView = UIView(frame: CGRect.zero)
+        DataManager.getGroups { (groups, error) in
+            if let groups = groups {
+                self.groups = groups
+            }
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    // MARK: - UITableViewDataSource
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return groups.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "GroupCell")!
+        let group = self.groups[indexPath.row]
+        cell.textLabel?.text = group.name
+        cell.imageView?.image = #imageLiteral(resourceName: "Logo")
+        group.getFile(forKey: #keyPath(Group.icon)) { (data) in
+            if let data = data {
+                cell.imageView?.image = UIImage(data: data)
+                cell.layoutIfNeeded()
+            }
+        }
+        
+        return cell
+    }
+    
+    // MARK: - UITableViewDelegate
+
+}
